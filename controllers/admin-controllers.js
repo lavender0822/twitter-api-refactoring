c2518e2f645d71127fbac83dd3628c4865f89970
lavender <id0421942@gmail.com>
2022-04-20 17:21:08 +0800

    調整 admin-controllers

--- controllers/admin-controllers.js
+++ controllers/admin-controllers.js
@@ -1,76 +1,67 @@
 const { User, Tweet, Like } = require('../models')
-const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken")
 
 const adminController = {
   login: (req, res, next) => {
-    const errData = req.user.data;
     try {
+      const errData = req.user.data;
       if (!errData) {
-        const userData = req.user.toJSON();
+        const userData = req.user.toJSON()
         if (userData.role === 'admin') {
           delete userData.password;
-          const token = jwt.sign(userData, process.env.JWT_SECRET, {
-            expiresIn: "30d",
-          }); // 簽發 JWT，效期為 30 天
+          const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: "30d" })
           res.json({
             status: "success",
             data: {
               token,
               user: userData,
             }
-          });
-        } else { res.json({ status: "error", message: "You are not admin!"}) }
+          })
+        } else { 
+          throw new Error("You are not admin!")
+        }
       } else {
         res.json(errData);
       }
-    } catch (err) {
-      next(err);
-    }
+    } catch (err) { next(err) }
   },
 
   getUsers: async (req, res, next) => {
-    return User.findAll({
-      attributes: { exclude: ['password'] },
-      include: [
-        { model: Tweet, attributes: ['id'], include: { model: Like, attributes: ['id'] } },
-        { model: User, as: 'Followings', attributes: ['id'] },
-        { model: User, as: 'Followers', attributes: ['id'] }
-      ]
-    })
-      .then(users => {
-        const result = users
-          .map(u => ({
-            ...u.toJSON()
-          }))
-
-        result.forEach(r => {
-          r.TweetsCount = r.Tweets.length
-          r.FollowingsCount = r.Followings.length
-          r.FollowersCount = r.Followers.length
-          r.TweetsLikedCount = r.Tweets.reduce((acc, tweet) => acc + tweet.Likes.length, 0)
-          delete r.Tweets
-          delete r.Followings
-          delete r.Followers
-        })
-
-        result.sort((a, b) => {
-          if (a.TweetsCount === b.TweetsCount) return (a.id - b.id)
-        return (b.TweetsCount - a.TweetsCount)
-        })
-        return res.json(result)
+    try{
+      const users = await User.findAll({
+        attributes: { exclude: ['password'] },
+        include: [
+          { model: Tweet, attributes: ['id'], include: { model: Like, attributes: ['id'] } },
+          { model: User, as: 'Followings', attributes: ['id'] },
+          { model: User, as: 'Followers', attributes: ['id'] }
+        ]
       })
-      .catch(err => next(err))
+      const result = users.map(u => ({...u.toJSON()}))
+      result.forEach(r => {
+        r.TweetsCount = r.Tweets.length
+        r.FollowingsCount = r.Followings.length
+        r.FollowersCount = r.Followers.length
+        r.TweetsLikedCount = r.Tweets.reduce((acc, tweet) => acc + tweet.Likes.length, 0)
+        delete r.Tweets
+        delete r.Followings
+        delete r.Followers
+      })
+      result.sort((a, b) => {
+      if (a.TweetsCount === b.TweetsCount) return (a.id - b.id)
+      return (b.TweetsCount - a.TweetsCount)
+      })
+      res.json(result)
+    } catch (e) { next(e) }
   },
 
-  deleteTweet: (req, res, next) => {
-    return Tweet.findByPk(req.params.id)
-      .then((tweet) => {
-        if (!tweet) return res.json({ status: 'error', message: "Tweet didn't exist!" })
-        return tweet.destroy();
-      })
-      .then((deleteTweet) => res.json({ status: "success", deleteTweet }))
-      .catch((err) => next(err));
-  },
+  deleteTweet: async (req, res, next) => {
+    try{
+      const tweet = await Tweet.findByPk(req.params.id)
+      if (!tweet) throw new Error("Tweet didn't exist!")
+      await tweet.destroy()
+      res.json({ status: 'success'})
+    } catch(e) { next(e) }
+  }
 };
 
 module.exports = adminController;
